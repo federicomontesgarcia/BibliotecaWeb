b24cc689f86e5b9a2892f513248e4170a45a8383
federicomontesgarcia <federicomontesgarcia@gmail.com>
2024-07-17 20:53:00 -0500

    configuraciones adicionales

--- libros/urls.py
+++ libros/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('editar_libro/<int:id_libro>/', views.editar_libro, name='editar_libro'),
     path('save-book-edit', views.save_book_edit, name='grabar_editar_libro'),
     
-    path('', views.LibroViews.as_view(), name="libro"),
+    path('v1', views.LibroViews.as_view(), name="libro"),
     path('libro/<int:id>/', views.LibroViews.as_view()),
     path('autor/', views.AutorViews.as_view(), name="autor"),
     path('autor/<int:id>/', views.AutorViews.as_view()),
